700b4580a973e82c2ef65cd2d8de532a86b20c93
Johannes Ackermann <johannes.ackermann@oxid-esales.com>
2019-05-07 14:22:15 +0200

    Added new phpdoc headers

--- Events.php
+++ Events.php
@@ -1,6 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
+ * This Software is the property of OXID eSales and is protected
+ * by copyright law - it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * @author        OXID Academy
+ * @link          https://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2019
+ *
  * User: michael
  * Date: 23.04.19
  * Time: 08:24
--- metadata.php
+++ metadata.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This Software is the property of OXID eSales and is protected
+ * by copyright law - it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * @author        OXID Academy
+ * @link          https://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2019
+ */
 
 /**
  * Metadata version
