d3075d0c41dc290d130c2e3a65a2e4e2e3c22446
Michael Keiluweit <michael.keiluweit@oxid-esales.com>
2019-05-09 15:14:45 +0200

    Finished working on the first version of the integration test.

--- Tests/Integration/ModulesInteractionTest.php
+++ Tests/Integration/ModulesInteractionTest.php
@@ -18,13 +18,12 @@
 
 namespace OxidAcademy\OxCoin\Tests\Integration;
 
-use OxidEsales\Eshop\Application\Controller\PaymentController;
-use OxidEsales\Eshop\Application\Model\User;
+use OxidEsales\Eshop\Core\Field;
+use OxidEsales\Eshop\Core\Model\BaseModel;
 use OxidEsales\Eshop\Core\Module\Module;
-use OxidEsales\TestingLibrary\ModuleLoader;
-use OxidEsales\TestingLibrary\UnitTestCase;
+use OxidEsales\Eshop\Core\Module\ModuleList;
 
-class ModulesInteractionTest extends UnitTestCase
+class ModulesInteractionTest extends \OxidEsales\TestingLibrary\UnitTestCase
 {
     /**
      * The example module oxacFeeFreePayment will kick out this example module oxCoin from the payment list as oxCoin
@@ -32,30 +31,27 @@ class ModulesInteractionTest extends UnitTestCase
      * To simulate the integration tests we can switch if the FeeFreePayment module should be active or not. If it is
      * active, the Integration test will fail (correctly).
      *
-     * @param bool $activateFeeFreePayment
+     * @throws \Exception
      */
     protected function helperActivateAllModules($activateFeeFreePayment = false)
     {
-        // Get the module Directory
         $moduleDirectory = $this->getConfig()->getModulesDir();
+        $moduleList = oxNew(ModuleList::class);
+        $listOfAllModules = $moduleList->getModulesFromDir($moduleDirectory);
 
-        //Build a list with all modules
-        $moduleList = oxNew(\OxidEsales\Eshop\Core\Module\ModuleList::class);
-        $modules = $moduleList->getModulesFromDir($moduleDirectory);
+        foreach ($listOfAllModules as $module) {
 
-
-        // Build an array with the vendor id and the module id. (e.g. oe/gdproptin)
-        $modulesToActivate = [];
-        foreach ($modules as $module) {
             /** @var Module $module */
-            if ($module->getId() != 'oxacfeefreepayments') {
-                $modulesToActivate[] = $module->getModulePath();
+            if (
+                !$module->isActive() &&
+                (
+                    $module->getId() != 'oxacfeefreepayments' ||
+                    $activateFeeFreePayment
+                )
+            ) {
+                parent::_getModuleLoader()->installModule($module->getModulePath());
             }
         }
-
-        // Activate all collected modules
-        $moduleLoader = new ModuleLoader();
-        $moduleLoader->activateModules($modulesToActivate);
     }
 
     public function testPaymentListProvidedByThePaymentController()
@@ -65,15 +61,59 @@ class ModulesInteractionTest extends UnitTestCase
 
         $_POST['sShipSet'] = 'oxidstandard';
 
-        // Creating a user object for the payment list
-        $user = oxNew(User::class);
-        $user->load('oxdefaultadmin');
+        // <Creating Shipping Cost Rules>
+        $delivery = oxNew(\OxidEsales\Eshop\Application\Model\Delivery::class);
+        $delivery->setId('_delivery_test_id');
+        $delivery->oxdelivery__oxactive = new Field(1);
+        $delivery->oxdelivery__oxparamend = new Field(999);
+        $delivery->save();
 
-        $controller = oxNew(PaymentController::class);
+        $del2delset = oxNew(BaseModel::class);
+        $del2delset->init('oxdel2delset');
+        $del2delset->oxdel2delset__oxdelid = new Field($delivery->getId());
+        $del2delset->oxdel2delset__oxdelsetid = new Field('oxidstandard');
+        $del2delset->save();
+        // </Creating Shipping Cost Rules>
+
+
+        // Assigning the payment to the shipping method oxidstandard.
+        $delset2payment = oxNew(BaseModel::class);
+        $delset2payment->init('oxobject2payment');
+        $delset2payment->oxobject2payment__oxpaymentid = new Field('oxcoin');
+        $delset2payment->oxobject2payment__oxobjectid = new Field('oxidstandard');
+        $delset2payment->oxobject2payment__oxtype = new Field('oxdelset');
+        $delset2payment->save();
+
+
+        // <Creating a basket>
+        $price = oxNew(\OxidEsales\Eshop\Core\Price::class);
+        $price->setPrice(1.0);
+
+        $article = oxNew(\OxidEsales\Eshop\Application\Model\Article::class);
+        $article->setId('test_665');
+        $article->setPrice($price);
+        $article->save();
+
+        $basket = oxNew(\OxidEsales\Eshop\Application\Model\Basket::class);
+        $basket->addToBasket($article->getId(), 1.0);
+
+        \OxidEsales\Eshop\Core\Registry::getSession()->setBasket($basket);
+        // </Creating a basket>
+
+
+        // Creating a user
+        $user = oxNew(\OxidEsales\Eshop\Application\Model\User::class);
+        $user->setId('_user_test_id');
+        $user->oxusers__oxusername = new Field('_user_test_username');
+        $user->save();
+
+
+        // Creating a PaymentController object and inject the user object.
+        $controller = oxNew(\OxidEsales\Eshop\Application\Controller\PaymentController::class);
         $controller->setUser($user);
 
 
-
+        // From all payments at least our payment method oxcoin must be in the list.
         $ids = [];
         foreach ($controller->getPaymentList() as $payment) {
             $ids[] = $payment->oxpayments__oxid->value;
